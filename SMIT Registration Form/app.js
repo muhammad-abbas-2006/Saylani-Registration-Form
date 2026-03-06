1b50be6abbb692503f68fa7f850de91efe221971
Muhammad Abbas <muhammadabuabbas907@gmail.com>
2026-03-07 01:05:42 +0500

    Update app.js

--- SMIT Registration Form/app.js
+++ SMIT Registration Form/app.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'https://grdpokfywpqvporuxfry.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyZHBva2Z5d3BxdnBvcnV4ZnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzODc3NTEsImV4cCI6MjA2OTk2Mzc1MX0.vMX2To9Sa3ltrlnyOOZ_DGAO-tMhVumX08qiEQ2k6P8'
+const supabaseUrl = ''
+const supabaseKey = ""
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 // Student Portal work start
@@ -71,3 +71,4 @@ else{
 }
 
 });
+
